17a07d1a0e4811186a0182dfe418f8cdd088e95e
richitirta <richitirta@windowslive.com>
2022-02-08 17:32:54 +0700

    Update tarik_mesin.php

--- tarik_mesin.php
+++ tarik_mesin.php
@@ -1,13 +1,13 @@
 <?php
 include("config.php");
-$IP="192.168.89.5";
-$Key="0";
+$IP="192.168.89.5"; //Isi IP Mesin Absensi X401
+$Key="0"; //Isi Key Mesin Absensi X401 || Defaultnya 0
 if($IP=="") $IP="192.168.89.5"; 
-if($Key=="") $Key="0"; 
+if($Key=="") $Key="0";  
 
 
-    $Connect = fsockopen($IP, "80", $errno, $errstr, 1);
-    if($Connect){
+    $Connect = fsockopen($IP, "80", $errno, $errstr, 1); //Untuk open port dari ip mesin absensi
+    if($Connect){ //Jika Connect 
         $soap_request="<GetAttLog>
                             <ArgComKey xsi:type=\"xsd:integer\">".$Key."</ArgComKey>
                             <Arg><PIN xsi:type=\"xsd:integer\">All</PIN></Arg>
@@ -25,7 +25,7 @@ if($Key=="") $Key="0";
             $buffer=$buffer.$Response;
         }
     }else echo "Koneksi Gagal";
- $buffer=Parse_Data($buffer,"<GetAttLogResponse>","</GetAttLogResponse>");
+ $buffer=Parse_Data($buffer,"<GetAttLogResponse>","</GetAttLogResponse>");  //Parsing Data
     $buffer=explode("\r\n",$buffer);
     for($a=0;$a<count($buffer);$a++){
         $data=Parse_Data($buffer[$a],"<Row>","</Row>");
@@ -33,17 +33,17 @@ if($Key=="") $Key="0";
         $pin=Parse_Data($data,"<PIN>","</PIN>");
         $datetime=Parse_Data($data,"<DateTime>","</DateTime>");
         $status=Parse_Data($data,"<Status>","</Status>");
-$cekdulu= "select * from rekap where id='$pin' and waktu='$datetime' ";
+$cekdulu= "select * from rekap where id='$pin' and waktu='$datetime' "; //Cek Duplikasi Pada Sistem
 $niss1 = "select nis from siswa where id_siswa= '$pin'";
  $prosescek= mysqli_query($koneksi,$cekdulu);
   // if ($prosescek) { //proses mengingatkan data sudah ada
   // echo "<script>alert('Username Sudah Digunakan');history.go(-1) </script>";
   // }
   // else { //proses menambahkan data, tambahkan sesuai dengan yang kalian gunakan
-   $sql = "INSERT INTO rekap (id, waktu, nis, status, tanggal) values ('$pin','$datetime','$niss1',$status', '$datetime')";
+   $sql = "INSERT INTO rekap (id, waktu, nis, status, tanggal) values ('$pin','$datetime','$niss1',$status', '$datetime')"; //Insert Data Mesin Absensi ke Sistem 
    mysqli_query($koneksi,$sql);
   //}
-  ini_set('max_execution_time', 300);
+  ini_set('max_execution_time', 300); //Durasi MAX Eksekusi
      
     }
 echo "<script>alert('Selesai tarik data absen'); </script>";
